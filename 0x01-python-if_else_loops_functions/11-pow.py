a4ea732f28b5501b4b988b61c19150c2f093ae4f
DexteriousClyde <clydebilam@gmail.com>
2022-08-24 02:18:29 +0000

    task11

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def pow(a, b):
     res = 1
-    for i in range(b):
-        res *= a
+    if b > 0:
+        for i in range(b):
+            res *= a
+    else:
+        for i in range(abs(b)):
+            res *= 1/a
     return res
