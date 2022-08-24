030676f8b41f08aa816bf1d3ce7bcfe35a1742c2
DexteriousClyde <clydebilam@gmail.com>
2022-08-24 01:53:37 +0000

    fixed indent error

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,9 +5,9 @@ str1 = "Last digit of " + str(number) + " is "
 int1 = (abs(number) % 10)
 str1 += str(int1) + " "
 if int1 > 5:
-	str1 += "and is greater than 5"
+    str1 += "and is greater than 5"
 elif int1 == 0:
-	str1 += "and is 0"
+    str1 += "and is 0"
 else:
-	str1 += "and is less than 6 and not 0"
-print(str1 + '\n')
+    str1 += "and is less than 6 and not 0"
+print(str1, end='\n')
