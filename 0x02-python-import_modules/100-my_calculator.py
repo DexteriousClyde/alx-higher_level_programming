36e4ea4d6dcfeeb0fc05818cfbd0b2a159e1a834
DexteriousClyde <clydebilam@gmail.com>
2022-08-25 22:56:38 +0000

    hayini, kasi ni fela yini

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     arr = [add, sub, mul, div]
     for i, j in enumerate(mth):
         if argv[2] == j:
-            print("{} {} {} = {}".format(a, s, b, arr[i](a, b)))
+            print("{} {} {} = {}".format(a, j, b, arr[i](a, b)))
             break
     else:
         print("Unknown operator. Available operators: +, -, * and /")
