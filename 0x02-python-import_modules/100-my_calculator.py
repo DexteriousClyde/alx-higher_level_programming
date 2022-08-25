a94f52ce3208ae30a9b9e219ccb83f23adc0eb14
DexteriousClyde <clydebilam@gmail.com>
2022-08-25 22:51:32 +0000

    d

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
+if __name__ == "__main__":
     from sys import argv
     from calculator_1 import add, sub, mul, div
     if len(argv) != 4:
-        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
-        exit(1)
-    elif argv[2] not in '+-*/':
-        print('Unknown operator. Available operators: +, -, *, and /')
-        exit(1)
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        quit(1)
     a = int(argv[1])
     b = int(argv[3])
-    if argv[2] is '+':
-        print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
-    elif argv[2] is '-':
-        print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
-    elif argv[2] is '*':
-        print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
-    elif argv[2] is "/":
-        print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
+    mth = ["+", "-", "*", "/"]
+    arr = [add, sub, mul, div]
+    for i, j in enumerate(mth):
+        if argv[2] == j:
+            print("{} {} {} = {}".format(a, s, b, funcs[i](a, b)))
+            break
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        quit(1)
