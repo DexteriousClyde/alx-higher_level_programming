44e7b919414682a8c1bc0935fd6b381dfebf955d
DexteriousClyde <clydebilam@gmail.com>
2022-08-29 23:28:35 +0000

    Fix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for c in matrix:
         for r in c:
-            print("{:d}".format(i), end="")
+            print("{:d}".format(r), end="")
         print()
