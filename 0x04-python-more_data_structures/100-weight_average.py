0ce237c6189f29f46321776f5fc9f6241ecea4e4
DexteriousClyde <clydebilam@gmail.com>
2022-09-03 02:33:24 +0000

    hayi thyaka xem

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    num = (sum(x[0] * x[1] for x in my_list)
-    num2 = (sum(y[1] for y in my_list) or 1))
-    return num/num2
+    num = (sum(x[0] * x[1] for x in my_list)/(sum(y[1] for y in my_list) or 1))
+    return num
