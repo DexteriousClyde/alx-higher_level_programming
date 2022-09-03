0f6b655b873a7ef4cd38d7c3d77b2aac1ea010ac
DexteriousClyde <clydebilam@gmail.com>
2022-09-03 02:12:46 +0000

    thyaka

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@ def roman_to_int(roman_string):
     if type(roman_string) != str or roman_string == None:
         return 0;
     ret = 0
-    num = = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    for i in reversed(roamn_string):
-        ret += num[i] if total < num[i] * 5 else -num[i]
+    num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    for i in reversed(roman_string):
+        ret += num[i] if ret < num[i] * 5 else -num[i]
     return ret
