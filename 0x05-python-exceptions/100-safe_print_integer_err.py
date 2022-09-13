162b0fb1e05aafa694d81a059a4b9f953f0c0c1a
DexteriousClyde <clydebilam@gmail.com>
2022-09-13 02:34:30 +0000

    swona

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,5 +5,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except Exception as err:
-        print("Exception: {}".format(err), file=sys.stderr)
+        print("Exception: {}".format(sys.exc_info()[1])), file=sys.stderr)
         return False
